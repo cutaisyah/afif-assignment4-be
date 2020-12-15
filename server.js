c55aba3be3a94e1764cdb7a6f2b3750eb0613324
Louis Yupiter Consolatio <consolatio123@gmail.com>
2020-12-15 10:23:27 +0700

    fix server

--- server.js
+++ server.js
@@ -12,7 +12,54 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
+const normalizePort = val => {
+    var port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+  
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+  
+    return false;
+  };
+  
+  const onError = error => {
+    if (error.syscall !== "listen") {
+      throw error;
+    }
+    const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+    switch (error.code) {
+      case "EACCES":
+        console.error(bind + " requires elevated privileges");
+        process.exit(1);
+        break;
+      case "EADDRINUSE":
+        console.error(bind + " is already in use");
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  };
+  
+  const onListening = () => {
+    const addr = server.address();
+    const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+    debug("Listening on " + bind);
+  };
 
-app.listen(port, ()=>{
+
+app.set("port", port);
+const server = http.createServer(app);
+server.on("error", onError);
+server.on("listening", onListening);
+server.listen(port);
+
+server.listen(port, ()=>{
     console.log(`App runs on http://localhost:${port}`);
 });
